0d7e0d46ce315d54c5a434a72378ac16e99bfb4b
len <67599880+lblake1994@users.noreply.github.com>
2021-12-30 13:05:43 -0500

    Added Dinner to menu drop downs.

---
+++ Cooking/Controllers/DinnerController.cs
@@ -0,0 +1,25 @@
+﻿using Cooking.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cooking.Controllers
+{
+    public class DinnerController : Controller
+    {
+        // GET: Lunch
+        public ActionResult Index()
+        {
+
+            DinnerDto model = new DinnerDto();
+            model.name = "Dinner";
+
+
+            return View(model);
+
+        }
+
+    }
+}
---
+++ Cooking/Models/DinnerDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cooking.Models
+{
+    public class DinnerDto
+    {
+        public string name { get; set; }
+
+    }
+}
